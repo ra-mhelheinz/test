8ae9f0bff95f1ed5c1a89fd55c0cdb815e949da6
Mhel Heinz Debalucos <killerswar2@github.com>
2018-04-26 16:15:28 +0800

    Added Login

--- index.php
+++ index.php
@@ -5,6 +5,12 @@
 	<title>Sample</title>
 </head>
 <body>
-	
+	<div >
+		<form>
+			<label>Login</label>
+			<input type="text" name="username" placeholder="Username">
+			<input type="password" name="" placeholder="Password">
+		</form>
+	</div>
 </body>
 </html>
